a417fb856298d7a71b02f7a2a31208ff492ac22a
LAPTOP-TSHKM7PV\吉田千夏 <chinatsu1072@keio.jp>
2024-05-16 11:43:21 +0900

    audio

--- audioSetting.js
+++ audioSetting.js
@@ -1,39 +1,20 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const ctx = new AudioContext();
 
-let sampleSource;
-// 再生中のときはtrue
-let isPlaying = false;
+const audioElement = document.querySelector("audio");
+// Web Audio API内で使える形に変換
+const track = ctx.createMediaElementSource(audioElement);
 
-// 音源を取得しAudioBuffer形式に変換して返す関数
-async function setupSample() {
-  const response = await fetch("./audioFile/eejyanaika.mp3");
-  const arrayBuffer = await response.arrayBuffer();
-  // Web Audio APIで使える形式に変換
-  const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
-  return audioBuffer;
-}
-
-// AudioBufferをctxに接続し再生する関数
-function playSample(ctx, audioBuffer) {
-  sampleSource = ctx.createBufferSource();
-  // 変換されたバッファーを音源として設定
-  sampleSource.buffer = audioBuffer;
+document.querySelector("#play").addEventListener("click", () => {
+  if (ctx.state === "suspended") {
+    ctx.resume();
+  }
   // 出力につなげる
-  sampleSource.connect(ctx.destination);
-  sampleSource.start();
-  isPlaying = true;
-}
-
-document.querySelector("#play").addEventListener("click", async () => {
-  // 再生中なら二重に再生されないようにする
-  if (isPlaying) return;
-  const sample = await setupSample();
-  playSample(ctx, sample);
+  track.connect(ctx.destination);
+  audioElement.play();
 });
 
-// oscillatorを破棄し再生を停止する
-document.querySelector("#stop").addEventListener("click", async () => {
-  sampleSource?.stop();
-  isPlaying = false;
+// audioElementを一時停止する
+document.querySelector("#pause").addEventListener("click", () => {
+  audioElement.pause();
 });
