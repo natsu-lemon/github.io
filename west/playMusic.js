529e67bcc45ced6e975fc7ed75546657be839b44
LAPTOP-TSHKM7PV\吉田千夏 <chinatsu1072@keio.jp>
2024-05-16 14:59:07 +0900

    shige add playMusic

--- west/playMusic.js
+++ west/playMusic.js
@@ -1,20 +1,42 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const ctx = new AudioContext();
 
+
 const audioElement = document.querySelector("audio");
 // Web Audio API内で使える形に変換
 const track = ctx.createMediaElementSource(audioElement);
 
-document.querySelector("#play").addEventListener("click", () => {
-  if (ctx.state === "suspended") {
-    ctx.resume();
-  }
-  // 出力につなげる
-  track.connect(ctx.destination);
-  audioElement.play();
+// artoolkitのコンテキストを作成
+var arToolkitContext = new THREEx.ArToolkitContext();
+
+
+// ARフレームワークによって提供されるイベントハンドラーを定義
+arToolkitSource.onResize = function() {
+    arToolkitSource.onResizeElement();
+    arToolkitSource.copyElementSizeTo(renderer.domElement);
+    if (arToolkitContext.arController !== null) {
+        arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas);
+    }
+};
+
+// ARフレームワークによって提供されるコールバックを定義
+arToolkitContext.arController.addEventListener('markerFound', function(event) {
+    // マーカーが見つかったときの処理
+    if (event.data.markerId === 'shige') {
+        // #shigeマーカーが認識されたときの処理をここに記述する
+        if (ctx.state === "suspended") {
+            ctx.resume();
+        }
+  	// 出力につなげる
+  	track.connect(ctx.destination);
+  	audioElement.play();
+    }
 });
 
-// audioElementを一時停止する
-document.querySelector("#pause").addEventListener("click", () => {
-  audioElement.pause();
+arToolkitContext.arController.addEventListener('markerLost', function(event) {
+    // マーカーが見失われたときの処理
+    if (event.data.markerId === 'shige') {
+        // #shigeマーカーが見失われたときの処理をここに記述する
+        audioElement.pause();
+    }
 });
